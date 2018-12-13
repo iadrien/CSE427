c4f1a27163090272749c66c5f37009518ec2d945
iadrien <xie.z@wustl.edu>
2018-12-12 22:53:38 -0600

    Progress

---
+++ Final Project/k-means-master/Milestone2/step1.py
@@ -0,0 +1,13 @@
+import sys
+import re
+from pyspark import SparkContext
+from pyspark import SparkConf
+
+sc = SparkContext()
+# read from local filesystem
+data = sc.textFile("file:/home/cloudera/training_materials/dev1/data/devicestatus.txt")
+
+d = data.map(lambda l: l.split(',')).filter(lambda l : len(l) == 14).map(lambda l : (l[12], l[13], l[0], l[1].split(' ')[0], l[1].split(' ')[1])).filter(lambda l : l[0] != '0' and l[1] != '0').map(lambda l : l[0] + "," +  l[1] + "," + l[2] + "," + l[3] + "," + l[4])
+
+d.saveAsTextFile("hdfs:///loudacre/devicestatus_etl")
+sc.stop()
